8cdb8f782cb3741f980c66ecb4f292aaf40858b4
nouhben <nouhben@MacBook-Pro.local>
2023-09-23 07:51:11 +0100

    starting a new ui 5

--- lib/main.dart
+++ lib/main.dart
@@ -32,77 +32,80 @@ class OboardingScreen extends StatelessWidget {
     return Scaffold(
       extendBodyBehindAppBar: true,
       extendBody: true,
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Container(
-            height: MediaQuery.of(context).size.height * .6,
-            width: 200,
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage(
-                  'assets/images/onboarding.jpg',
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              height: MediaQuery.of(context).size.height * .6,
+              width: 200,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/images/onboarding.jpg'),
+                  fit: BoxFit.cover,
                 ),
-                fit: BoxFit.cover,
-              ),
-              borderRadius: BorderRadius.all(
-                Radius.circular(32.0),
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(36.0),
+                  bottomRight: Radius.circular(64.0),
+                ),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                const SizedBox(height: 32.0),
-                Text(
-                  'Explore your journey \nonly with us',
-                  maxLines: 2,
-                  softWrap: true,
-                  textAlign: TextAlign.center,
-                  overflow: TextOverflow.fade,
-                  style: Theme.of(context)
-                      .textTheme
-                      .headlineLarge!
-                      .copyWith(color: Colors.black, fontSize: 36),
-                ),
-                const SizedBox(height: 32.0),
-                Text(
-                  'All your vacations distinations are here. Enjoy your holiday.',
-                  maxLines: 3,
-                  softWrap: true,
-                  textAlign: TextAlign.center,
-                  overflow: TextOverflow.fade,
-                  style: Theme.of(context)
-                      .textTheme
-                      .displaySmall!
-                      .copyWith(color: Colors.black, fontSize: 16),
-                ),
-                const SizedBox(height: 32.0),
-                SizedBox(
-                  width: double.maxFinite,
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.blueAccent,
-                      elevation: 4.0,
-                      padding: const EdgeInsets.symmetric(vertical: 16.0),
-                    ),
-                    child: Text(
-                      'Get Started',
-                      textAlign: TextAlign.center,
-                      style: Theme.of(context)
-                          .textTheme
-                          .bodyLarge!
-                          .copyWith(color: Colors.white),
-                    ),
+            // Image.asset(
+            //   'assets/images/onboarding.jpg',
+            // ),
+
+            const SizedBox(height: 32.0),
+            Text(
+              'Explore your journey \nonly with us',
+              maxLines: 2,
+              softWrap: true,
+              textAlign: TextAlign.center,
+              overflow: TextOverflow.fade,
+              style: Theme.of(context)
+                  .textTheme
+                  .headlineLarge!
+                  .copyWith(color: Colors.black, fontSize: 36),
+            ),
+            const SizedBox(height: 32.0),
+            Text(
+              'All your vacations distinations are here. Enjoy your holiday.',
+              maxLines: 3,
+              softWrap: true,
+              textAlign: TextAlign.center,
+              overflow: TextOverflow.fade,
+              style: Theme.of(context)
+                  .textTheme
+                  .displaySmall!
+                  .copyWith(color: Colors.black, fontSize: 16),
+            ),
+            //const SizedBox(height: 36.0),
+            const Spacer(),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 48.0),
+              child: SizedBox(
+                width: double.maxFinite,
+                height: 46,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blueAccent,
+                    elevation: 4.0,
+                    padding: const EdgeInsets.symmetric(vertical: 16.0),
+                  ),
+                  child: Text(
+                    'Get Started',
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context)
+                        .textTheme
+                        .bodyLarge!
+                        .copyWith(color: Colors.white),
                   ),
                 ),
-              ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
